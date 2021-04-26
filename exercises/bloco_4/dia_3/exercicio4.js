9cbccc67a50f0fa387bbdcffb49376613ce42585
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-04-26 16:16:12 -0300

    Adicioando restriçoes ao exercicio 4

--- exercises/bloco_4/dia_3/exercicio4.js
+++ exercises/bloco_4/dia_3/exercicio4.js
@@ -1,27 +1,34 @@
-let n = 7;
+let n = 9;
 
-for(let index = 1; index <= n; index += 2) {
+if (n % 2 !== 0) {
+    for(let index = 1; index <= n; index += 2) {
     
-    let linha = "";
-
-    for (let espaco1 = 1; espaco1 <= ((n - index)/2); espaco1 += 1) {
-
-        linha += " ";
-
+        let linha = "";
+    
+        for (let espaco1 = 1; espaco1 <= ((n - index)/2); espaco1 += 1) {
+    
+            linha += " ";
+    
+        }
+    
+        for (let index2 = 1; index2 <= index; index2 += 1 ) {
+    
+            linha += "*";
+    
+        }
+    
+        for (let espaco2 = 1; espaco2 <= ((n - index)/2); espaco2 += 1) {
+    
+            linha += " ";
+    
+        }
+    
+        console.log(linha);
     }
 
-    for (let index2 = 1; index2 <= index; index2 += 1 ) {
+} else {
 
-        linha += "*"
+    console.log("Numero da base tem que ser ímpar");
 
-    }
-
-    for (let espaco2 = 1; espaco2 <= ((n - index)/2); espaco2 += 1) {
-
-        linha += " ";
-
-    }
-
-
-    console.log(linha);
 }
+
