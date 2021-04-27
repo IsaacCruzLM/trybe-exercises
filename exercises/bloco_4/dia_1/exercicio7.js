029e178ed06fde2767a37148d754ab4832491c3e
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-04-27 16:13:48 -0300

    Refatoração dos exercicio 7, 8 e 9 do dia 1, bloco 4

--- exercises/bloco_4/dia_1/exercicio7.js
+++ exercises/bloco_4/dia_1/exercicio7.js
@@ -3,14 +3,20 @@ let b = 11;
 let c = 13;
 let maiorValor;
 
-if (a >= b && a >= c) {
-    maiorValor = a;
-}
-else if (b >= a && b >= c) {
-    maiorValor = b;
-}
-else {
-    maiorValor = c;
+function maiorValue (a, b, c) {
+
+    if (a >= b && a >= c) {
+        maiorValor = a;
+    }
+    else if (b >= a && b >= c) {
+        maiorValor = b;
+    }
+    else {
+        maiorValor = c;
+    }
+
+    return maiorValor;
+
 }
 
-console.log(maiorValor);
+console.log(maiorValue(a, b, c));
