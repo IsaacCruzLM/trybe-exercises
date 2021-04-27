38b361a70392b2f5558fec29d1e92383439c9f0e
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-04-27 16:07:19 -0300

    Refatoração do exercicio 1 ao exercicio 6 do dia1, bloco 4

--- exercises/bloco_4/dia_1/exercicio2.js
+++ exercises/bloco_4/dia_1/exercicio2.js
@@ -1,6 +1,9 @@
 let a = 10;
 let b = 26;
 
-let subtracao = a - b;
 
-console.log(subtracao);
+function sub(a, b) {
+    return a - b;
+}
+
+console.log(sub(a, b));
--- exercises/bloco_4/dia_1/exercicio4.js
+++ exercises/bloco_4/dia_1/exercicio4.js
@@ -1,6 +1,9 @@
 let a = 10;
 let b = 26;
 
-let divisao = a / b;
 
-console.log(divisao);
+function div(a, b) {
+    return a / b;
+}
+
+console.log(div(a, b));
