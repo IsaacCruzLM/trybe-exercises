b5445448d5689a389f13d60b9493d15d617aa9e0
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-05-13 11:29:29 -0300

    Adiciona validação de data

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -13,5 +13,62 @@ function gerarEstadosNoForm () {
 
 gerarEstadosNoForm();
 
-// Validação do ano
+// Validação da data
 
+let picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'D/M/YYYY',
+    toString(date, format) {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        // dateString is the result of `toString` method
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    }
+});
+
+// Validação dos campos
+
+new window.JustValidate('.your-form', {
+    Rules: {
+      email: {
+          required: true,
+          email: true
+      },
+      checkbox: {
+          required: true
+      },
+      name: {
+          required: true,
+          minLength: 3,
+          maxLength: 15
+      },
+      text: {
+          required: true,
+          maxLength: 300,
+          minLength: 5
+      },
+      password: {
+          required: true,
+          password: true,
+          minLength: 4,
+          maxLength: 8
+      },
+      zip: {
+          required: true,
+          zip: true
+      },
+      phone: {
+          phone: true
+      }
+  }
+});
