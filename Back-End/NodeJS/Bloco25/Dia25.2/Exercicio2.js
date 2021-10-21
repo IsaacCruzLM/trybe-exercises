6aa671479f0bf39587db20724c0d56916b93f283
Isaac Cruz <isaac.clm1@gmail.com>
2021-10-21 19:08:13 -0300

    Exercicio 3

--- Back-End/NodeJS/Bloco25/Dia25.2/Exercicio2.js
+++ Back-End/NodeJS/Bloco25/Dia25.2/Exercicio2.js
@@ -1,13 +1,16 @@
 const func1 = require('./Exercicio1');
 
-const func2 = () => {
+const func2 = async () => {
     const n1 = Math.floor(Math.random() * 100 + 1);
     const n2 = Math.floor(Math.random() * 100 + 1);
     const n3 = Math.floor(Math.random() * 100 + 1);
 
-    func1(n1, n2, n3)
-    .then(valor => console.log(valor))
-    .catch(error => console.log(error));
+    try {
+        const result = func1(n1, n2, n3);
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 func2();
