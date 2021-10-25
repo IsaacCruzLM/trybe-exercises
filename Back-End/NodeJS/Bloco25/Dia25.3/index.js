47b6858f8cb99f7842da4718d0688dc876ccc233
Isaac Cruz <isaac.clm1@gmail.com>
2021-10-25 19:15:00 -0300

    ADD: Exercicio 3

--- Back-End/NodeJS/Bloco25/Dia25.3/index.js
+++ Back-End/NodeJS/Bloco25/Dia25.3/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PingPong = require('./controllers/Exercicio1');
 const HelloName = require('./controllers/Exercicio2');
+const Grettings = require('./controllers/Exercicio3');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/ping', PingPong);
 app.post('/hello', HelloName);
+app.post('/greetings', Grettings);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
